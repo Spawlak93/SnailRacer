73973d20873a66294fc4a80497afdab75ae7b09d
Spawlak93 <63370588+Spawlak93@users.noreply.github.com>
2020-09-28 08:20:02 -0400

    Add project files.

---
+++ SnailRacer/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SnailRacer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Random _rand = new Random();
+            int _RaceLength = 1000;
+            int _NumbaerOfRacers = 6;
+            List<string> _SnailNames = new List<string> { "Sir. Es Car Vango", "Speedy Shelldon", "Slowdo Baggins", "Ronald Speedsly", "The Phantom Mollsk", "Jennifer Slowpez", "Albus Dumblesnore", "Gastro-pod-Racer" };
+
+            Console.Write("Enter the name of your snail:");
+            SnailClass PlayerSnail = new SnailClass(Console.ReadLine(), _rand);
+
+            var PlayerOneRace = PlayerSnail.RaceStart(_RaceLength);
+
+            
+            var Podium = new List<SnailClass>();
+            var RaceTasks = new List<Task<SnailClass>> { PlayerOneRace };
+            for (int i = 0; i < _NumbaerOfRacers; i++)
+            {
+                var snail = new SnailClass(_SnailNames[_rand.Next(_SnailNames.Count)], _rand);
+                _SnailNames.Remove(snail.Name);
+                RaceTasks.Add(snail.RaceStart(_RaceLength));
+                Thread.Sleep(2);
+            }
+
+            while (RaceTasks.Count > 0)
+            {
+                Task<SnailClass> finishedTask = await Task.WhenAny(RaceTasks);
+                FinishedRace(Podium.Count + 1, finishedTask.Result.Name);
+                Podium.Add(finishedTask.Result);
+                RaceTasks.Remove(finishedTask);
+            }
+            Console.WriteLine("And the results are in!\n" +
+                "Press any key to continue.");
+            Console.ReadKey();
+            Console.Clear();
+            DrawPodium(Podium);
+            Console.WriteLine();
+            Console.ReadLine();
+
+        }
+
+        public static void FinishedRace(int finished, String name)
+        {
+            switch(finished)
+            {
+                case 1:
+                    Console.WriteLine($"{name} Has finished the race in 1st place");
+                    break;
+                case 2:
+                    Console.WriteLine($"{name} Has finished the race in 2nd place");
+                    break;
+                case 3:
+                    Console.WriteLine($"{name} Has finished the race in 3rd place");
+                    break;
+                default:
+                    Console.WriteLine($"{name} Has finished the race in {finished}th place");
+                    break;
+            }
+        }
+
+        public static void DrawPodium(List<SnailClass> snails)
+        {
+            Console.WriteLine($"\n\n\n\n\n\n\n" +
+                string.Format($"{ 0, - 41 }",  snails[0].Name )); 
+        }
+    }
+}
---
+++ SnailRacer/SnailClass.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SnailRacer
+{
+    public class SnailClass
+    {
+        private readonly int _MaxSpeedChange = 4;
+        private readonly int _MaxSpeed = 10;
+        private readonly int _MinSpeed = 1;
+        private Random _rand;
+        public SnailClass(string name, Random random)
+        {
+            _rand = random;
+            Name = name;
+            if (_rand.Next(5) == 0 || Name == "Albus Dumblesnore")
+            {
+                Speed = 0;
+            }
+            else
+                Speed = _rand.Next(3,7);
+        }
+        public string Name { get; set; }
+        public int Speed { get; set; }
+
+        private void SpeedChange(int i)
+        {
+            if (Speed == 0)
+            {
+                Console.WriteLine($"{Name} Woke up with a surge of energy");
+                Speed = 7;
+                return;
+            }
+            //40% chance to go slower
+            //10% chance of falling asleep
+            if(i == 0)
+            {
+                Speed = 0;
+                Console.WriteLine($"{Name} fell asleep");
+            }
+            else if (i < 5)
+            {
+                Speed -= _rand.Next(1, _MaxSpeedChange);
+                Console.WriteLine($"{Name} slowed down");
+                if (Speed <= _MinSpeed)
+                {
+                    Console.WriteLine($"Is {Name} even still moving?");
+                    Speed = _MinSpeed;
+                }
+                return;
+            }
+            //50 % chance to go faster
+            else
+            {
+                Speed += _rand.Next(1, _MaxSpeedChange);
+                Console.WriteLine($"{Name} sped up");
+                if (Speed > _MaxSpeed)
+                {
+                    Console.WriteLine($"{Name} couldnt go any faster");
+                    Speed = _MaxSpeed;
+                }
+                return;
+            }
+
+        }
+
+        public async Task<SnailClass> RaceStart(int raceLength)
+        {
+            double firstQuarter = raceLength *.75;
+            double halfway = raceLength * .5;
+            double finalQuarter = raceLength * .25;
+                
+            if (Speed == 0git remote add origin https://github.com/Spawlak93/SnailRacer.git
+            git branch -M master
+            git push - u origin master)
+            {
+                Console.WriteLine($"{Name} decided to take a nap instead");
+                await Task.Delay(3000);
+                SpeedChange(10);
+            }
+            Console.WriteLine($"{Name} has started the race");
+            while (raceLength > 0)
+            {
+                raceLength -= Speed;
+                if (raceLength < firstQuarter)
+                {
+                    Console.WriteLine($"{Name} is a quarter of the way there");
+                    firstQuarter = -9999;
+                }
+                if (raceLength < halfway)
+                {
+                    Console.WriteLine($"{Name} is halfway done!");
+                    halfway = -9999;
+                }
+                if (raceLength < finalQuarter)
+                {
+                    finalQuarter = -9999;
+                    Console.WriteLine($"{Name} is in the final quarter!");
+                }
+                //2 % chance of speedchange
+                if (_rand.Next(50) == 1)
+                    SpeedChange(_rand.Next(10));
+                await Task.Delay(300);
+            }
+            return this;
+        }
+    }
+}
